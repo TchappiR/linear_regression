bbe518a604e8c838ba50ef2750ce748e70346fcf
TchappiR <tchappiromaric@gmail.com>
2024-03-24 23:22:04 +0100

    add csv file

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("HEllo")
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data_df = pd.read_csv("./prix_maisons.csv")
+plt.figure(figsize=(15, 6))
+plt.plot(df["surface"], df["prix"], "bo")
+print(plt.show())
+
+
