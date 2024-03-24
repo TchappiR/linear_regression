fc9901998dc6609716e46717d86eeaddfa5008f2
TchappiR <tchappiromaric@gmail.com>
2024-03-24 23:24:52 +0100

    data visualisation

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data_df = pd.read_csv("./prix_maisons.csv")
-plt.figure(figsize=(15, 6))
-plt.plot(df["surface"], df["prix"], "bo")
+
+print(plt.figure(figsize=(15, 6)))
+print(plt.plot(data_df["surface"], data_df["prix"], "bo"))
 print(plt.show())
 
 
